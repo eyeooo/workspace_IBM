d35d35cec4ea971972acfc8fce84b4ecef8abac7
edison.lando <edison.lando@gmail.com>
2013-10-17 16:37:24 +0800

    workspace of IBM

---
+++ Test2/src/com/ibm/test/Test2.java
@@ -0,0 +1,13 @@
+package com.ibm.test;
+
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello ibm");
+	}
+
+}
